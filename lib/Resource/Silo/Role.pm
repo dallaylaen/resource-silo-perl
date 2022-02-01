04c52233e2d9065a6647331eed19c1e6bbe62e4c
Konstantin S. Uvarin <khedin@gmail.com>
2022-02-02 00:00:46 +0200

    [doc] Add some docs to role

--- lib/Resource/Silo/Role.pm
+++ lib/Resource/Silo/Role.pm
@@ -18,8 +18,23 @@ use strict;
 use warnings;
 use Moo::Role;
 
+# Avoid putting anything into role's namespace.
 use Resource::Silo qw();
 
+=head1 ATTRIBUTES
+
+=head2 res
+
+A lazy, read-only dependency container reference.
+
+Default = Resource::Silo::silo().
+
+=cut
+
+# Don't call it silo. It's possible (with enough care from R::S itself)
+# but will lead to enormous confusion and undebuggable code.
+# Should it be lazy?
+# Should it be writable?
 has res => is => 'ro', lazy => 1, builder => \&Resource::Silo::silo;
 
 1;
